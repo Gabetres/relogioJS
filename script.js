4fd0cffede22598c23df825ff1d8f700377365d2
Gabetres <70735135+Gabetres@users.noreply.github.com>
2023-04-05 09:52:31 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -10,6 +10,42 @@ function relogio() {
     const formatoMinutos = minutos.toString().padStart(2, "0");
     const formatoSegundos = segundos.toString().padStart(2, "0");
 
-   elementoRelogio.textContent = `${formatoHoras}:${formatoMinutos}:${formatoSegundos}`
+    const frase = document.querySelector(".frase");
+    dia = new Array('Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sabado');
+
+    mes = new Array('Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Stembro', 'Outubro', 'Novembro', 'Dezembro');
+
+    hoje = new Date;
+
+    frase.innerHTML = `${dia[hoje.getDay()]} , ${hoje.getDate().toString().padStart(2, "0")} de ${mes[hoje.getMonth()]} de ${hoje.getFullYear()} `
+
+    const esta = document.querySelector(".esta")
+
+    dianoite = new Array('Madrugada','Manhã', 'Tarde', 'Noite');
+
+    if (horas >= 0) {
+        esta.innerHTML = `Esta de ${dianoite[0]}`
+        html.classList.toggle('madrugada');
+    }
+
+    if(horas >= 6){
+        esta.innerHTML = `Esta de ${dianoite[1]}`
+        html.classList.toggle('manha');
+    }
+
+    if (horas >= 13) {
+        esta.innerHTML = `Esta de ${dianoite[2]}`
+        html.classList.toggle('tarde');
+    }
+
+    if (horas >= 18) {
+        esta.innerHTML = `Esta de ${dianoite[3]}`
+        html.classList.toggle('noite');
+    }
+
+
+
+
+    elementoRelogio.textContent = `${formatoHoras}:${formatoMinutos}:${formatoSegundos}`
 }
 setInterval(relogio, 1000);
